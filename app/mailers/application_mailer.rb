b99958a530e2837e75f2b8421f29f12fd4db6812
Admin <09artirajora@gmail.com>
2018-06-19 11:27:36 +0530

    change credentials and add confirmation link with confirmation mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-
-	def send_confirmation_mail(user)
-		@user = user
-		mail(:to => user.email, :subject => "Account activation")
-	end
-
-  
+	default from: 'from@example.com'
+	layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
     port: 587, 
     domain: 'gmail.com', 
     user_name: 'arti.svarogt@gmail.com',
-    password: 'svarogt@123',
+    password: 'arti.svarogt@123',
     authentication: 'plain', 
     enable_starttls_auto: true 
   }
